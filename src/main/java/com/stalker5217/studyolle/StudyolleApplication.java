db00c6aad4166c969b39d0cd3e2820a131a64739
Song <stalker5217@gmail.com>
2021-08-14 22:03:49 +0900

    first commit

---
+++ src/main/java/com/stalker5217/studyolle/StudyolleApplication.java
@@ -0,0 +1,13 @@
+package com.stalker5217.studyolle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyolleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyolleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stalker5217/studyolle/StudyolleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stalker5217.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyolleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
